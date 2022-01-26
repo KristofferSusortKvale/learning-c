78007b2af833ad9474159571b6ffc1b5d03f75d0
KristofferSusortKvale <kristoffersusortkvale@gmail.com>
2022-01-26 18:00:16 +0100

    Create hello-world.c
    Made a hello world in C

---
+++ hello-world/hello_world.c
@@ -0,0 +1,10 @@
+// Simple c hello world
+
+//header file for input and output
+#include <stdio.h>
+
+//main function
+int main(int argc, char const *argv[]) {
+  printf("%s\n", "hello world");
+  return 0;
+}
